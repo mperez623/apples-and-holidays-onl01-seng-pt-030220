e64e32fdbec034778b1eb82a94fd0e208f569633
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 22:43:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, hol|
-    puts "#{season.capitalize}:"
+    puts "#{season.capitalize}: "
     hol.each do |holiday, sup|
       puts "#{holiday.capitalize}: #{sup.join(", ")}"
     end
